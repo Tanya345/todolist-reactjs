33a796c933c33a2367d2f329d40a1f3a93ce50f9
tanya.bansal-at-326184364317 <tanya.bansal@flexiele.com>
2022-09-19 10:13:30 +0530

    first commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about component
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus dolor doloribus repellat vitae esse consequatur praesentium accusamus iste, sed temporibus doloremque hic dolore rerum, perferendis atque quas, ipsam ea nam minus obcaecati impedit eos.</p>
+        </div>
+    )
+}
---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const Footer = () => {
+    // let footerStyle = {
+    //     position: "relative",
+    //     top: "10vh",
+    //     width: "100%",
+    // }
+    return (
+        // <footer className="bg-dark text-light py-3" style={footerStyle}>
+        <footer className="bg-dark text-light py-3">
+            <p className="text-center">
+            Copyright &copy; MyTodosList.com
+            </p>
+            
+        </footer>
+    )
+}
